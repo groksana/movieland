fae0de69d821c05ffc6c7b6d8e794bb1843fe468
groksana <groksana@gmail.com>
2017-11-09 23:48:10 +0200

    [t-1] Optimize cache implementation

--- movieland-persistence-jdbc/src/main/java/com/gromoks/movieland/dao/cache/GenericCacheImpl.java
+++ movieland-persistence-jdbc/src/main/java/com/gromoks/movieland/dao/cache/GenericCacheImpl.java
@@ -17,48 +17,25 @@ public class GenericCacheImpl implements GenericCache<Genre>, GenreDao {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private List<Genre> cacheGenreList;
+    private volatile List<Genre> cacheGenreList;
 
     @Autowired
     private GenreDao genreDao;
 
-    private boolean writeLock = false;
-
-    public GenericCacheImpl() {
-        cacheGenreList = new ArrayList<>();
-    }
-
     public List<Genre> getAll() {
-        if (writeLock) {
-            synchronized (cacheGenreList) {
-                return cacheGenreList;
-            }
-        } else {
-            return cacheGenreList;
-        }
+        List<Genre> copy = cacheGenreList;
+        List<Genre> genres = new ArrayList<>();
+        genres.addAll(copy);
+        return genres;
     }
 
     @Override
     @PostConstruct
     @Scheduled(fixedRateString="${cache.fixedRate}")
     public void invalidate() {
-        writeLock = true;
-        synchronized (cacheGenreList) {
-            cleanup();
-            fill();
-        }
-        writeLock = false;
-    }
-
-    private void cleanup() {
-            log.info("Start cleanup of cached genres");
-            cacheGenreList.clear();
-            log.info("Finish cleanup of cached genres");
-    }
-
-    private void fill() {
         log.info("Start to fill genres to cache");
         cacheGenreList = genreDao.getAll();
         log.info("Finish to fill genres to cache. It obtains {} genres", cacheGenreList.size());
     }
+
 }
